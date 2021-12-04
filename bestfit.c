bf7c1f61e7c4d1a023af55a66ffaa233193325fb
Prad <vasudhevan090701@gmail.com>
2021-12-04 17:42:22 +0530

    bestfit

---
+++ bestfit.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct bestfit
+{
+    int size;
+    int assign;
+} bf;
+
+int main()
+{
+    int n, m;
+    printf("Enter no of processes");
+    scanf("%d", &n);
+    printf("Enter no of Holes");
+    scanf("%d", &m);
+
+    bf arr[n];
+
+    int x[n];
+    int y[n];
+
+    printf("Enter process size one by one");
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i].size);
+        arr[i].assign = -1;
+    }
+
+    printf("\nEnter the hole size one by one");
+
+    for (int i = 0; i < m; i++)
+    {
+        scanf("%d", &x[i]);
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        y[i] = x[i];
+    }
+
+    int touch=-1;
+    int index= -1;
+
+    for (int i = 0; i < n; i++)
+    {
+        touch = 999999;
+        index = -1;
+        
+        for (int j = 0; j < m; j++)
+        {
+            if (arr[i].size <= x[j])
+            {
+                if(touch>x[j])
+                {
+                    touch = x[j];
+                    index = j;
+                }                
+            }
+        }
+
+        if(touch==999999 || index==-1)
+        {
+            arr[i].assign = -1;
+        }
+        else
+        {
+            arr[i].assign = index+1;
+            x[index]-=arr[i].size;
+        }
+    }
+    
+    printf("Process\t\t\tHoles");
+    for (int i = 0; i < n; i++)
+    {
+        if(arr[i].assign==-1)
+        {
+            printf("\np%d\t\t\t%d(%d)\n", i + 1, 0,0);
+        }
+        else
+        printf("\np%d\t\t\t%d(%d)\n", i + 1, y[arr[i].assign - 1], x[arr[i].assign - 1]);
+    }
+
+    return 0;
+}
